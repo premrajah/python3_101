1bd1142a28a7a59596ea3cb554105024fa2d1f07
Prem Rajah <prajah@ctlondon.com>
2018-02-16 16:15:23 +0000

    end classes, start dat, time, datetime classes

--- 02_classes.py
+++ 02_classes.py
@@ -10,7 +10,29 @@ class myClass():
         print("myClass method2 ", someString)
 
 
+
+
+class anotherClass(myClass):
+    def method1(self):
+        myClass.method1(self)
+        print("anotherClass method1")
+    
+    def method2(self, someString):
+        print("anotherClass method2 ")
+
+
+
+
 def main():
+    c = myClass()
+    c.method1()
+    c.method2("This is a string arg")
+
+    print("---")
+
+    c2 = anotherClass()
+    c2.method1()
+    c2.method2("Hello Classes")
 
 
 if __name__ == "__main__":
